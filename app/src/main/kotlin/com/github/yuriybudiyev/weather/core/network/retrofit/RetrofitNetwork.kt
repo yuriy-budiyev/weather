4500a8dc438ebc869abb763ca1f8094d41c07695
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2025-11-14 10:11:44 +0300

    Dev

---
+++ app/src/main/kotlin/com/github/yuriybudiyev/weather/core/network/retrofit/RetrofitNetwork.kt
@@ -0,0 +1,47 @@
+package com.github.yuriybudiyev.weather.core.network.retrofit
+
+import com.github.yuriybudiyev.weather.core.network.data.ForecastResponse
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private interface WeatherService {
+
+    @GET("forecast.json")
+    suspend fun getWeather(
+        @Query("key")
+        key: String,
+        @Query("q")
+        query: String,
+        @Query("days")
+        days: Int,
+    ): ForecastResponse
+}
+
+@Singleton
+class RetrofitDataSource @Inject constructor() {
+
+    private val weatherService: WeatherService
+
+    init {
+        val networkJson = Json { ignoreUnknownKeys = true }
+        weatherService = Retrofit
+            .Builder()
+            .baseUrl("http://api.weatherapi.com/v1/")
+            .addConverterFactory(networkJson.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(WeatherService::class.java)
+    }
+
+    suspend fun getWeather(): ForecastResponse =
+        weatherService.getWeather(
+            key = "fa8b3df74d4042b9aa7135114252304",
+            query = "55.7569,37.6151",
+            days = 3,
+        )
+}
